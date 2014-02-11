3155043cf8fe1e9f858cf7fa5d381867d4f8f96e
Gal Cohen <galcohen26@gmail.com>
2014-02-11 15:27:10 -0500

    textkit tutorial

---
+++ ObjectiveC_and_iPhone/ios7/TextKitNotepad/TextKitNotepad/SyntaxHighlightTextStorage.h
@@ -0,0 +1,13 @@
+//
+//  SyntaxHighlightTextStorage.h
+//  TextKitNotepad
+//
+//  Created by Gal Cohen on 2/11/14.
+//  Copyright (c) 2014 Colin Eberhardt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SyntaxHighlightTextStorage : NSTextStorage
+- (void)update;
+@end
