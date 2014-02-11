697ee50acc010bd1e4c7123b678b4d3ccc7f2829
Gal Cohen <galcohen26@gmail.com>
2014-02-11 18:23:57 -0500

    git issues

---
+++ ObjectiveC_and_iPhone/ios7/pixate/pixate/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  pixate
+//
+//  Created by Gal Cohen on 2/11/14.
+//  Copyright (c) 2014 Gal Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ObjectiveC_and_iPhone/ios7/pixate/pixateTests/pixateTests.m
@@ -0,0 +1,34 @@
+//
+//  pixateTests.m
+//  pixateTests
+//
+//  Created by Gal Cohen on 2/11/14.
+//  Copyright (c) 2014 Gal Cohen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface pixateTests : XCTestCase
+
+@end
+
+@implementation pixateTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
